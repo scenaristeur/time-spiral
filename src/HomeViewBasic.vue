9f1d4cd24bee01ba902592d3dbb290b13d16d87a
Scenaristeur <scenaristeur@gmail.com>
2023-03-28 21:30:50 +0200

    init

---
+++ src/HomeViewBasic.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <div ref="graph"></div>
+  </div>
+</template>
+
+<script>
+import ForceGraph3D from '3d-force-graph';
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    //HelloWorld
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const N = 300;
+      const gData = {
+        nodes: [...Array(N).keys()].map(i => ({ id: i })),
+        links: [...Array(N).keys()]
+          .filter(id => id)
+          .map(id => ({
+            source: id,
+            target: Math.round(Math.random() * (id - 1))
+          }))
+      };
+
+      this.Graph = ForceGraph3D()(this.$refs.graph)
+        .graphData(gData);
+    }
+  }
+}
+</script>
